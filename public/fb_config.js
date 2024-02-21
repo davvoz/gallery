9592750cb15a46bf4855ded982a5576cbf66a08a
David <dav.baz@alice.it>
2024-02-21 23:07:31 +0100

    fix index deployed

---
+++ public/fb_config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8m3IyfgaH7_S3f0z_8j7Nf-ryoL5H3_g",
+    authDomain: "wada-42dd4.firebaseapp.com",
+    databaseURL: "https://wada-42dd4.firebaseio.com",
+    projectId: "wada-42dd4",
+    storageBucket: "wada-42dd4.appspot.com",
+    messagingSenderId: "87083060977",
+    appId: "1:87083060977:web:a525a46e147b0f0e7a2350"
+};
